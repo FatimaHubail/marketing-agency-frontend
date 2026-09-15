f52fb1f231bc17842574f3513785f8ddee1fe271
FatimaHubail <fhubail03@gmail.com>
2026-09-15 21:46:42 +0300

    Refactor campaign request service

--- src/services/campaignRequestService.js
+++ src/services/campaignRequestService.js
@@ -1,7 +1,46 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/campaign-requests`;
+const SERVER_URL = import.meta.env.VITE_BACK_END_SERVER_URL;
+const REQUESTS_URL = `${SERVER_URL}/requests`;
+const CAMPAIGN_REQUESTS_URL = `${SERVER_URL}/campaign-requests`;
 
+// client
+const createCampaignRequest = async (formData) => {
+  const res = await fetch(REQUESTS_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const data = await res.json();
+
+  if (data.err) {
+    throw new Error(data.err);
+  }
+
+  return data;
+};
+
+const getMyCampaignRequests = async () => {
+  const res = await fetch(REQUESTS_URL, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+
+  const data = await res.json();
+
+  if (data.err) {
+    throw new Error(data.err);
+  }
+
+  return data;
+};
+
+// staff
 const getCampaignRequests = async () => {
-  const res = await fetch(BASE_URL, {
+  const res = await fetch(CAMPAIGN_REQUESTS_URL, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
@@ -17,7 +56,7 @@ const getCampaignRequests = async () => {
 };
 
 const updateCampaignRequest = async (id, formData) => {
-  const res = await fetch(`${BASE_URL}/${id}`, {
+  const res = await fetch(`${CAMPAIGN_REQUESTS_URL}/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
@@ -36,7 +75,7 @@ const updateCampaignRequest = async (id, formData) => {
 };
 
 const acceptCampaignRequest = async (id, formData) => {
-  const res = await fetch(`${BASE_URL}/${id}/assign`, {
+  const res = await fetch(`${CAMPAIGN_REQUESTS_URL}/${id}/assign`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
@@ -55,7 +94,7 @@ const acceptCampaignRequest = async (id, formData) => {
 };
 
 const rejectCampaignRequest = async (id, rejectedReason) => {
-  const res = await fetch(`${BASE_URL}/${id}/reject`, {
+  const res = await fetch(`${CAMPAIGN_REQUESTS_URL}/${id}/reject`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
@@ -74,36 +113,11 @@ const rejectCampaignRequest = async (id, rejectedReason) => {
 };
 
 const getUsersByRole = async (role) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACK_END_SERVER_URL}/admin/users?role=${role}`,
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    }
-  );
-
-  const data = await res.json();
-
-  if (data.err) {
-    throw new Error(data.err);
-  }
-
-  return data;
-};
-
-const createCampaignRequest = async (formData) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACK_END_SERVER_URL}/requests`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-      body: JSON.stringify(formData),
-    }
-  );
+  const res = await fetch(`${SERVER_URL}/admin/users?role=${role}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
 
   const data = await res.json();
 
@@ -115,10 +129,11 @@ const createCampaignRequest = async (formData) => {
 };
 
 export {
+  createCampaignRequest,
+  getMyCampaignRequests,
   getCampaignRequests,
   updateCampaignRequest,
   acceptCampaignRequest,
   rejectCampaignRequest,
   getUsersByRole,
-  createCampaignRequest,
 };
