1bf2aac03cb86e4730ed2ef74240385f010d0811
mo-durazi <mo.durazi.106@gmail.com>
2026-09-16 12:29:25 +0300

    added the outsourceService

---
+++ src/services/outsourceService.js
@@ -0,0 +1,42 @@
+const SERVER_URL = import.meta.env.VITE_BACK_END_SERVER_URL;
+const OUTSOURCE_URL = `${SERVER_URL}/outsource`;
+
+const getOutsourceProfile = async (outsourceId) => {
+    const res = await fetch(`${OUTSOURCE_URL}/${outsourceId}`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+    });
+
+    const data = await res.json();
+
+    if (data.err) {
+        throw new Error(data.err);
+    }
+
+    return data;
+};
+
+const updateOutsourceProfile = async (formData) => {
+    const res = await fetch(OUTSOURCE_URL, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+
+    if (data.err) {
+        throw new Error(data.err);
+    }
+
+    return data;
+};
+
+export {
+    getOutsourceProfile,
+    updateOutsourceProfile,
+};
